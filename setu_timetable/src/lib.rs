8894145eb31353fb0d9e04dfc947ba0c60e61d55
Keith Butler <keithbutler2001@gmail.com>
2023-10-11 12:34:27 +0100

    added comments for future features

--- setu_timetable/src/lib.rs
+++ setu_timetable/src/lib.rs
@@ -1,15 +1,17 @@
 use std::error::Error;
+use std::io::Read;
 use std::thread;
 use std::time::Duration;
 use thirtyfour::{prelude::{WebDriverError, ElementWaitable}, By, DesiredCapabilities, WebDriver, WebElement};
 use url::Url;
 use serde::Serialize;
+use thirtyfour::prelude::ElementQueryable;
 
 pub async fn scrape_setu_timetable() -> Result<(), Box<dyn Error>> {
     let driver = initialize_driver().await?;
     let url = Url::parse("https://studentssp.wit.ie/Timetables/StudentGroupTT.aspx")?;
 
-    print!("{}", url);
+    println!("{}", url);
 
     driver.goto(url).await?;
     tokio::time::sleep(Duration::from_secs(2)).await;
@@ -33,10 +35,31 @@ async fn initialize_driver() -> Result<WebDriver, WebDriverError> {
 }
 
 async fn search_timetable(driver: &WebDriver) -> Result<(), WebDriverError> {
-// click_choose_place(driver).await?;
     // TODO Create config file
-    // TODO check config file for data
-    // TODO Config file
+    // Check of first time running the program
+    // If so ask the user to select an School
+    // Write to config
+    // Click school
+    // then ask the user to select an Department
+    // Write to config
+    // Click dept
+    // then ask the user to select an POS
+    // Write to config
+    // Click POS
+    // then ask the user to select an Group
+    // Write to config
+    // Click group
+
+    println!("Printing Schools");
+    if let Ok(options ) = driver.find_all(By::Css("#cboSchool")).await {
+            for num in 1..options.len() {
+                println!("{}. {:?}", &num, get_options(&num).await);
+        }
+    }
+    // for option in options {
+    //     println!("{:?}", option)
+    // }
+
     click_css(driver, "#cboSchool > option:nth-child(7)").await?;
     tokio::time::sleep(Duration::from_secs(1)).await;
     click_css(driver, "#CboDept > option:nth-child(3)").await?;
@@ -46,11 +69,8 @@ async fn search_timetable(driver: &WebDriver) -> Result<(), WebDriverError> {
     click_css(driver, "#CboStudParentGrp > option:nth-child(3)").await?;
     tokio::time::sleep(Duration::from_secs(1)).await;
 
-    // write_place(driver, place).await?;
-    //
-    // click_search_button(driver).await?;
     click_css(driver, "#BtnRetrieve").await?;   // Generate Timetable
-    click_css(driver, "#btnPrint").await?;      // Print To Pdf
+    // click_css(driver, "#btnPrint").await?;      // Print To Pdf
 
     Ok(())
 }
@@ -62,4 +82,19 @@ async fn click_css(driver: &WebDriver, css_location: &str) -> Result<(), WebDriv
         .await?.click().await?;
 
     Ok(())
+}
+
+// struct TimeTableDetails {
+//
+// }
+
+// impl TimeTableDetails {
+//
+// }
+async fn get_options(option: &WebElement, num: &usize) -> Result<String, WebDriverError> {
+    option
+        .find(By::Css(&*format!("#cboSchool > option:nth-child({})", num)))
+        .await?
+        .text()
+        .await
 }
